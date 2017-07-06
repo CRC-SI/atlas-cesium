1348233ff6d7aecfe2ca92be3d8fa1e0119c22b9
Alex Leith <aleith@crcsi.com.au>
2017-07-06 16:03:52 +1000

    import to crcsi

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 // Meteor package definition.
 Package.describe({
-  name: 'urbanetic:atlas-cesium',
+  name: 'crc4si:atlas-cesium',
   version: '0.9.0_5',
   summary: 'An AGI Cesium <cesiumjs.org> implementation of the Atlas rendering API.',
-  git: 'https://github.com/urbanetic/atlas-cesium.git'
+  git: 'https://github.com/crc-si/atlas-cesium.git'
 });
 
 Package.onUse(function(api) {
